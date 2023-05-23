1e3dfb85f9ceeeb8658f482d0d849d04614f16a5
Ciro Costa <ciro.fga@gmail.com>
2023-05-22 22:22:17 -0300

    Update index.js

--- AMBICIOSOS/ciclista/src/components/Input/index.js
+++ AMBICIOSOS/ciclista/src/components/Input/index.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
 
-export default function Input({ type, label, input, placeholder, onChange }) {
+export default function Input({ type, label, input, placeholder, onChange, helperText, showError }) {
+  const [error, setError] = useState(true)
+  const [inputValue, setInputValue] = useState('')
+  const [externalHelperText, setExternalHelperText] = useState('')
+
+  const handleShowError = () => {
+    setExternalHelperText('')
+    setError(false)
+  }
+  useEffect(()=>{
+    if (helperText && helperText.length > 0) {
+      setError(true)
+      setExternalHelperText(helperText)
+    }
+  },[helperText])
   return(
     <div className='input-container'>
       <label>{ label }</label>
-      <input min='1' type={ type || 'text' } value={ input } placeholder={ placeholder } onChange={ (e) => onChange(e.target.value) } />
+      <input style={{borderColor:error?'red':'inherit', borderWidth:error?'2px':'0px'}} value={inputValue} min='1' onFocus={handleShowError} type={ type || 'text' } placeholder={ placeholder } onChange={ (e) => {setInputValue(e.target.value);onChange(e.target.value)} } />
+      {externalHelperText.length > 0 && <p>{label} {externalHelperText}</p>}
     </div>
   );
 }
